831628a755664ccb0dd8c4d8323f87750d28e546
Angel Byun <abyun22@turing.edu>
2023-05-16 14:35:23 -0600

    Create vendors for a market and will display error code when an invalid market_id is inputted

--- app/controllers/api/v0/markets_controller.rb
+++ app/controllers/api/v0/markets_controller.rb
@@ -1,7 +1,6 @@
 class Api::V0::MarketsController < ApplicationController
   def index
     render json: MarketSerializer.new(Market.all)
-
   end
 
   def show
---
+++ spec/requests/api/v0/vendors_request_spec.rb
@@ -0,0 +1,43 @@
+require 'rails_helper'
+
+describe "Vendors API" do
+  it "has a ist of all vendors" do
+    @market_1 = create(:market)
+
+    create_list(:vendor, 5, market_ids: @market_1.id, credit_accepted: true)
+
+    get "/api/v0/markets/#{@market_1.id}/vendors"
+
+    expect(response).to be_successful
+
+    vendors = JSON.parse(response.body, symbolize_names: true)[:data]
+
+    expect(vendors.count).to eq(5)
+
+    vendors.each do |vendor|
+      expect(vendor).to have_key(:attributes)
+      expect(vendor[:attributes]).to be_a(Hash)
+
+      expect(vendor).to have_key(:id)
+      expect(vendor[:attributes]).to have_key(:name)
+      expect(vendor[:attributes]).to have_key(:description)
+      expect(vendor[:attributes]).to have_key(:contact_name)
+      expect(vendor[:attributes]).to have_key(:contact_phone)
+      expect(vendor[:attributes]).to have_key(:credit_accepted)
+    end
+  end
+
+  it "displays error message if invalid id is passed" do
+    @market_1 = create(:market)
+
+    create_list(:vendor, 5, market_ids: @market_1.id, credit_accepted: true)
+
+    get "/api/v0/markets/10989/vendors"
+
+    expect(response).to_not be_successful
+
+    vendors = JSON.parse(response.body, symbolize_names: true)
+
+    expect(vendors).to eq({"error": "Couldn't find Market with 'id'=10989"})
+  end
+end
